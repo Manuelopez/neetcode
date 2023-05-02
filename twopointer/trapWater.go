e236916bfb192f0799cacfcd80d4970292280fc3
Manuel Lopez <mp.arturolopez@gmail.com>
2023-05-02 07:28:50 -0400

    finnished two pointers neetcode

---
+++ twopointer/trapWater.go
@@ -0,0 +1,28 @@
+package twopointer
+
+func Trap(height []int) int {
+    if len(height) == 0{
+        return 0
+    }
+
+    l, r := 0, len(height) - 1
+    leftMax, rightMax := height[l], height[r]
+
+    res := 0
+    for l < r{
+        if leftMax < rightMax{
+            l += 1
+            if (leftMax < height[l]){
+                leftMax = height[l]
+            }
+            res += leftMax - height[l]
+        }else{
+            r -= 1
+            if (rightMax < height[r]){
+                rightMax = height[r]
+            }
+
+            res += rightMax - height[r]
+        }
+    }
+    return res
